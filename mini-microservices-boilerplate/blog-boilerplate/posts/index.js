81fbf54500c97adf9f82996931e5d1d8e60f0d6f
Koushik Deb <koushik.imon@gmail.com>
2024-06-15 13:09:40 +0600

    Quick post and comment post and get api done

--- mini-microservices-boilerplate/blog-boilerplate/posts/index.js
+++ mini-microservices-boilerplate/blog-boilerplate/posts/index.js
@@ -1,15 +1,26 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const { randomBytes } = require('crypto')
+
 
 const app = express()
-
-app.use(express.json())
+app.use(bodyParser.json())
 
 const posts = {}
 
 app.get('/posts', (req, res)=>{
+    res.status(200).send(posts)
 })
 
 app.post('/posts', (req, res)=>{
+    const id = randomBytes(4).toString('hex')
+    const { title } = req.body
+
+    posts[id] = {
+        id, title
+    }
+
+    res.status(201).send(posts[id])
 })
 
 app.listen(4000, ()=>{
